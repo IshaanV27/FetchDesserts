f20f1cf18297468166d35802acc987c39a02b74d
Ishaan Verma <ishaan.verma99@gmail.com>
2023-06-04 16:14:26 -0400

    Initial Commit

---
+++ FetchDesserts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FetchDesserts
+//
+//  Created by Ishaan Verma on 6/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FetchDesserts/FetchDessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertsApp.swift
+//  FetchDesserts
+//
+//  Created by Ishaan Verma on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
